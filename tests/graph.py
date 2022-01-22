b5c850e4272cf6c9c5f382b5ab156fcc1ff9e135
Bruno <blemos@ua.pt>
2022-01-22 18:48:17 +0000

    interface done

--- tests/graph.py
+++ tests/graph.py
@@ -6,7 +6,6 @@ from cv2 import exp
 import kivy
 kivy.require("1.9.1")
 from kivy.app import App
-from kivy.cache import Cache
 from kivy.uix.image import Image
 from kivy.core.image import Image as CoreImage
 from kivy.uix.button import Button
@@ -14,47 +13,41 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
-graph1 = Image(source='graph1.png')
-graph2 = Image(source='graph2.png')
-
 class Calculator(App):
     def build(self):
         #image
-        global graph1
-        global graph2
         superBox = BoxLayout(orientation ='vertical')
         HB = BoxLayout(orientation ='horizontal')
+        self.graph1 = Image(source='graph1.png')
+        self.graph2 = Image(source='graph2.png')
         VB = GridLayout()
         VB.cols = 1
         self.btn = Button(text="refresh",size_hint = (0.2,0.2),bold = True,background_color = "#00FFCE")
         self.btn.bind(on_press=self.callback)
         VB.add_widget(self.btn)
-        HB.add_widget(graph1)
-        HB.add_widget(graph2)
+        HB.add_widget(self.graph2)
+        HB.add_widget(self.graph1)
         superBox.add_widget(HB)
         superBox.add_widget(VB)
-        print(graph1)
-        print(graph2)
         return superBox
     def callback(self,instance):
-        global graph1
-        global graph2
-        print("START")
+        print("ok")
         cpuinfo = random.randrange(1,100)
         x = [cpuinfo,100-cpuinfo]
         y = ["CPU in use","CPU free"]
         plt.pie(x,labels=y)
-        plt.savefig("graph1")
+        plt.savefig('graph1')
         plt.close()
+
         meminfo = random.randrange(1,100)
         x = [meminfo,100-meminfo]
         y = ["MEM in use","MEM free"]
         plt.pie(x,labels=y)
         plt.savefig("graph2")
         plt.close()
-        graph1 = Image(source='graph1.png')
-        graph2 = Image(source='graph2.png')
-        print("DO IT")
-        self.refresh()
+        self.graph1.source = 'graph1.png'
+        self.graph2.source = 'graph2.png'
+        self.graph2.reload()
+        self.graph1.reload()
 
 Calculator().run()
