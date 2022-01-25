bf26fd1e8199dab0c0fe928b1c0da7616c10bb1d
jp-amaral <jp.amaral@ua.pt>
2022-01-25 01:45:30 +0000

    Send info auto

--- tests/graph.py
+++ tests/graph.py
@@ -1,4 +1,5 @@
 import socket, sys, psutil, random, kivy
+from turtle import color
 import pandas as pd
 from matplotlib import pyplot as plt
 
@@ -32,24 +33,46 @@ class Graph(App):
         self.graph1 = Image(source='graph1.png')
         self.graph2 = Image(source='graph2.png')
         VB = GridLayout()
-        VB.cols = 1
-        self.btn = Button(text="Send info",size_hint = (0.2,0.2),bold = True,background_color = "#00FFCE")
-        self.btn.bind(on_press=self.sendInfo)
-        VB.add_widget(self.btn)
+        VB.cols = 2
+        self.btn1 = Button(text="Send info",size_hint = (0.2,0.2),bold = True,background_color = "#c2d4ff", color="#ffffff")
+        self.btn2 = Button(text="Send info automatically",size_hint = (0.2,0.2),bold = True,background_color = "##9cffd4", color="#ffffff")
+        self.btn1.bind(on_press=self.sendInfo)
+        self.btn2.bind(on_press=self.sendInfoAuto)
+        VB.add_widget(self.btn1)
+        VB.add_widget(self.btn2)
         HB.add_widget(self.graph2)
         HB.add_widget(self.graph1)
         superBox.add_widget(HB)
         superBox.add_widget(VB) 
         self.time = 0
+        self.auto = False
+        self.sent = False
 
         Clock.schedule_interval(self.update, 0.1)
         
         return superBox
 
+    def sendInfoAuto(self, instance):
+        self.auto = not self.auto
+        
+        if self.auto:
+            self.btn2.text="Sending... (click to stop)"
+            self.btn2.background_color="#ff9c9c"
+        else:
+            self.btn2.text="Send info automatically"
+            self.btn2.background_color="##9cffd4"
+
     def update(self, *args):
+
+        if self.time%2!=0:
+           self.sent = False 
+
+        if self.auto and (self.time%2==0) and (not self.sent):
+            self.sent = True
+            self.sendInfo(self.auto)
+
         #Generating info
-        self.time = Clock.get_time()
-        print(self.time)
+        self.time = int(Clock.get_time())
         cpuinfo = psutil.cpu_percent()
     
         #Saving CPU info
@@ -76,7 +99,7 @@ class Graph(App):
         Window.size = (800, 600)
 
 
-    def sendInfo(self,instance):
+    def sendInfo(self, instance):
         try:
             print("Sending info...")
             data = str(psutil.cpu_percent()) + "/" + str(psutil.virtual_memory().percent)
